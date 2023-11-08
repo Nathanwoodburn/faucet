7172f448d6a7ddb33bd891e0d51cbf8eb0ed78e6
Nathan Woodburn <github@nathan.woodburn.au>
2023-11-08 12:32:05 +1100

    feat: Add domain buying and gifting

--- gift.py
+++ gift.py
@@ -1,10 +1,19 @@
 import os
 import dotenv
 import json
+import requests
+import pyotp
+import time
+
 
 dotenv.load_dotenv()
 loaded = False
 gifts = []
+nbcookie = os.getenv('cookie')
+cookies = {"namebase-main": nbcookie}
+nb_endpoint = "https://www.namebase.io/"
+
+max_price = 4 # Max price to buy a domain at (in HNS)
 
 
 def gift(name,email,referer, ip):
@@ -50,5 +59,57 @@ def gift(name,email,referer, ip):
     # Save the file
     with open(path, 'w') as f:
         json.dump(gifts, f)
+
+    headers = {"Accept": "application/json", "Content-Type": "application/json"}
+    params = {"recipientEmail": email, "senderName": "Woodburn Faucet",
+              "note": "Enjoy your free domain! - Woodburn Faucet"}
+
+    names = requests.get(nb_endpoint + "/api/user/domains/owned?offset=0&sortKey=acquiredAt&sortDirection=desc&limit=100"
+                         ,headers=headers, cookies=cookies)
+    if names.status_code != 200:
+        return "Error getting names:<br>" + names.text
+
+    names = names.json()
+    if len(names['domains']) == 0:
+        domains_market = requests.get(nb_endpoint + "/api/domains/marketplace?offset=0&buyNowOnly=true&sortKey=price&sortDirection=asc&exclude=%2Cnumbers%2Chyphens%2Cunderscores&maxLength=10&offersOnly=false"
+                         ,headers=headers, cookies=cookies)
+        if domains_market.status_code != 200:
+            return "Error getting names:<br>" + domains_market.text
+        
+        domains_market = domains_market.json()
+        if len(domains_market['domains']) == 0:
+            return "No domains available to gift<br>Check back in a few minutes"
+        
+        domain = domains_market['domains'][0]['name']
+        print("Buying: " + domain,flush=True)
+        price = int(domains_market['domains'][0]['ammount'])
+        if price > max_price*1000000:
+            return "Domain price too high<br>Check back in a few minutes"
+        
+
+
+        payload = {
+            "listingId": domains_market['domains'][0]['id']
+        }
+        buy = requests.post(nb_endpoint + "/api/v0/marketplace/"+domain+"/buynow",headers=headers,data=json.dumps(payload), cookies=cookies)
+        if buy.status_code != 200:
+            return "Error buying name:<br>" + buy.text
+        
+
+    else:
+        domain = names['domains'][0]['name']
+    
+    print("Gifting: " + domain,flush=True)
+
+    # Add this name to gifts record
+    gifts[-1]['domain'] = domain
+
+
+
+    send_name = requests.post(nb_endpoint + "/api/gift/" + domain.strip(),headers=headers,data=json.dumps(params), cookies=cookies)
+
+    if send_name.status_code != 200:
+        return "Error sending gift:<br>" + send_name.text
+
 
     return True
